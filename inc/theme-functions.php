0c26a7c79081fa69642335c24cdd84548b90776e
moonbyt3 <milorad.jekic@deptagency.com>
2022-03-20 04:21:11 +0100

    Fixes ai1 export node_modules ignore

--- inc/theme-functions.php
+++ inc/theme-functions.php
@@ -223,6 +223,6 @@ if( function_exists('acf_add_options_page') ) {
 
 add_filter( 'ai1wm_exclude_themes_from_export',
 function ( $exclude_filters ) {
-  $exclude_filters[] = 'victoriakliniken_video/node_modules';
+  $exclude_filters[] = 'victoriakliniken-video/node_modules';
   return $exclude_filters;
 } );
